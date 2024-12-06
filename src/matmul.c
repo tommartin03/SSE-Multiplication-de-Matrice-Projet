1b9a73fd50f6f65969c9ebbfdd7868e7557797c5
Allard Noe <noe.allard@etu.univ-orleans.fr>
2024-12-06 15:42:21 +0100

    ADD: Version moins naive

--- src/matmul.c
+++ src/matmul.c
@@ -13,6 +13,20 @@ void naive_mul(float *A, float *B, float *res, size_t dim) {
   }
 }
 
+void moins_naive_mul(float *A, float *B, float *res, size_t dim) {
+    for (size_t i = 0; i < dim; i++) {
+        for (size_t j = 0; j < dim; j++) {
+            res[i * dim + j] = 0;
+        }
+        for (size_t k = 0; k < dim; k++) {
+            float premier = A[i * dim + k];
+            for (size_t j = 0; j < dim; j++) {
+                res[i * dim + j] += premier * B[k * dim + j];
+            }
+        }
+    }
+}
+
 void check(float *A, float *B, float *res, size_t dim) {
   float *exp = malloc(dim * dim * sizeof(float));
 
@@ -40,7 +54,18 @@ int main() {
   }
 
   // your code or function here
+
+  clock_t start = clock();
   naive_mul(A, B, C, dim);
+  clock_t end = clock();
+  double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+  printf("Temps d'execution de naive_mul: %f\n", time_spent);
+
+  start = clock();
+  moins_naive_mul(A, B, C, dim);
+  end = clock();
+  double time_spent2 = (double)(end - start) / CLOCKS_PER_SEC;
+  printf("Temps d'execution de moins_naive_mul: %f\n", time_spent2);
 
 // you can activate check by adding -DCHECK_MUL to your command line
 #ifdef CHECK_MUL
