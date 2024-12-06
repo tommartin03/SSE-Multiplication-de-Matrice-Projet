4a4013b03160ccbe82a71cc8e728c8484c5ed529
Allard Noe <noe.allard@etu.univ-orleans.fr>
2024-12-06 15:17:07 +0100

    Add: native multiplication de matrice

--- src/matmul.c
+++ src/matmul.c
@@ -2,12 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 
-void naive_mul(float *A, float *B, float *res) {}
+void naive_mul(float *A, float *B, float *res, size_t dim) {
+  for (size_t i = 0; i < dim; i++) {
+    for (size_t j = 0; j < dim; j++) {
+      res[i * dim + j] = 0;
+      for (size_t k = 0; k < dim; k++) {
+        res[i * dim + j] += A[i * dim + k] * B[k * dim + j];
+      }
+    }
+  }
+}
 
 void check(float *A, float *B, float *res, size_t dim) {
   float *exp = malloc(dim * dim * sizeof(float));
 
-  naive_mul(A, B, exp);
+  naive_mul(A, B, exp, dim);
 
   for (size_t i = 0; i < dim * dim; i++) {
     if (exp[i] != res[i]) {
@@ -17,6 +26,8 @@ void check(float *A, float *B, float *res, size_t dim) {
 }
 
 int main() {
+  //gcc -o mat matmul.c -DCHECK_MUL -O2
+
   size_t dim = 2048;
 
   float *A = malloc(dim * dim * sizeof(float));
@@ -29,10 +40,11 @@ int main() {
   }
 
   // your code or function here
+  naive_mul(A, B, C, dim);
 
 // you can activate check by adding -DCHECK_MUL to your command line
 #ifdef CHECK_MUL
-  check(A, B, C);
+  check(A, B, C, dim);
 #endif
 
   free(A);
